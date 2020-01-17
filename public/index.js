2e6c7c5d93f960dd3638d3de41a0919709ca8d8a
Ramprasad-R <ramprasad.info@gmail.com>
2020-01-17 21:05:26 +0100

    Added error handling to api fetch function

--- public/index.js
+++ public/index.js
@@ -1,8 +1,13 @@
 
 async function getSWApiData() {
-  const response = await fetch("https://swapi.co/api/people/1/");
-  const parsedResponse = await response.json();
-  document.querySelector(".displaySWName").innerHTML = `<p> ${parsedResponse.name} </p>`
+  try {
+    const response = await fetch("https://swapi.co/api/people/1/");
+    const parsedResponse = await response.json();
+    document.querySelector(".displaySWName").innerHTML = `<p> ${parsedResponse.name} </p>`
+  }
+  catch (err) {
+    console.log(err);
+  }
 }
 
 getSWApiData();
