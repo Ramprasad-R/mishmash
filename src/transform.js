0de3758c3d563d8526575227c37e982620d4ad13
Ramprasad-R <ramprasad.info@gmail.com>
2020-01-18 00:43:02 +0100

    updated groupAdultsByAgeRange function to categorize data based on age group

--- src/transform.js
+++ src/transform.js
@@ -1,31 +1,21 @@
 const groupAdultsByAgeRange = (Array) => {
   return Array.reduce((categoryByAge, currentPerson) => {
-    if(currentPerson.age > 18 && currentPerson.age <= 20){
-      categoryByAge['20 and younger'].push(currentPerson)
+    if(currentPerson.age >= 18 && currentPerson.age <= 20){
+      categoryByAge['20 and younger'] === undefined ? categoryByAge['20 and younger'] = [currentPerson] : categoryByAge['20 and younger'].push(currentPerson)
     }
     if(currentPerson.age > 20 && currentPerson.age <= 30){
-      categoryByAge['21-30'].push(currentPerson)
+      categoryByAge['21-30'] === undefined ? categoryByAge['21-30'] = [currentPerson] : categoryByAge['21-30'].push(currentPerson)
     }
     if(currentPerson.age > 30 && currentPerson.age <= 40){
-      categoryByAge['31-40'].push(currentPerson)
+      categoryByAge['31-40'] === undefined ? categoryByAge['31-40'] = [currentPerson] : categoryByAge['31-40'].push(currentPerson)
     }
     if(currentPerson.age > 40 && currentPerson.age <= 50){
-      categoryByAge['41-50'].push(currentPerson)
+      categoryByAge['41-50'] === undefined ? categoryByAge['41-50'] = [currentPerson] : categoryByAge['41-50'].push(currentPerson)
     }
     if(currentPerson.age > 50){
-      categoryByAge['51 and older'].push(currentPerson)
+      categoryByAge['51 and older'] === undefined ? categoryByAge['51 and older'] = [currentPerson] : categoryByAge['51 and older'].push(currentPerson)
     }
     return categoryByAge;
   },{})
 }
-
 module.exports = {groupAdultsByAgeRange}
-
-
-// - `20 and younger`
-//     - `21-30`
-//     - `31-40`
-//     - `41-50`
-//     - `51 and older`
-
-// '20 and younger' : [], '21-30' : [], '31-40' : [], '41-50' : [], '51 and older' : [] 
