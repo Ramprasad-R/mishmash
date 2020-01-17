7f1b15838f13e24cd631586a7428327af56e115f
Ramprasad-R <ramprasad.info@gmail.com>
2020-01-17 23:13:49 +0100

    Added function to get, add, clear, and total the cart

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,7 +1,31 @@
 module.exports = class ShoppingCart {
   constructor(){
-
+    this.currentCart = [];
+  }
+  getItems(){
+    return this.currentCart
+  }
+  addItem(itemName, quantity, price){
+    const currentCartItem = {}
+    currentCartItem.name = itemName;
+    currentCartItem.quantity = quantity;
+    currentCartItem.pricePerUnit = price;
+    this.currentCart.push(currentCartItem)
+  }
+  clear(){
+    this.currentCart = [];
+  }
+  total(){
+    return this.currentCart.reduce((total, item) => {
+      return total + (item.pricePerUnit * item.quantity)
+    }, 0)
   }
 }
 
 //module.exports.ShoppingCart = ShoppingCart;
+
+// {
+//   name: "Trash can",
+//   quantity: 1,
+//   pricePerUnit: 15.5
+// }
